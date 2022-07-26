4508fb00845f5e513f56b3a00ff8684595559ef2
jorgenav <jorgen.avdal@ntnu.no>
2022-07-26 21:53:10 +0200

    Redid setpathsscript

--- examples/FLUST/setPathsScript.m
+++ examples/FLUST/setPathsScript.m
@@ -1,13 +1,10 @@
-% for git repos, you may want to copy this file, use the copy in FLUST scripts, then let
-% git ignore the copy.
-
 lastwarn(''); % reset warning
 
 %% set paths here
 
 % PSF generators (Field II/MUST/other)
 addpath('PathToField'); % Path to Field II goes here
-% addpath('PathToMUST'); % Path to MUST if this is used
+addpath('PathToMUST'); % Path to MUST if this is used
 
 % addpath('C:\Users\jorgenav\Software\field_IIpro\m_files');
 
@@ -20,5 +17,5 @@ addpath('..\..'); % ustb main folder
 if strcmp( msgid, 'MATLAB:mpath:nameNonexistentOrNotADirectory')
     disp('At least one addpath statement in setPathsScript returned with a warning.')
     edit setPathsScript.m
-    error('Check that paths to PSF simulator (Field/MUST/other) are set properly in setPathsScript.m');
+    warning('Check that paths to PSF simulator (Field/MUST/other) are set properly in setPathsScript.m');
 end
