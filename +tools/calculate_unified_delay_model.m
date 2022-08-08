9876408138f951e2d4afb3180d7614f4765a1542
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2022-08-08 15:39:21 +0200

    Correcting unified delayed model

--- +tools/calculate_unified_delay_model.m
+++ +tools/calculate_unified_delay_model.m
@@ -4,21 +4,30 @@ function [transmit_delay_out] = calculate_unified_delay_model(transmit_delay_tem
 
 try
     % Reshape the delays into the size of the scan
-    if isa(scan,'uff.linear_scan')
+    if isa(scan,'uff.linear_scan') || isa(scan, 'uff.steered_scan')
         tx_delay = reshape(transmit_delay_temp,scan.N_z_axis,scan.N_x_axis);
         x_matrix = reshape(scan.x,scan.N_z_axis,scan.N_x_axis);
         z_matrix = reshape(scan.z,scan.N_z_axis,scan.N_x_axis);
+        dirtest = scan.x_axis(end)-scan.x_axis(1);
         N_lines = scan.N_x_axis;
     elseif isa(scan,'uff.sector_scan')
         tx_delay = reshape(transmit_delay_temp,scan.N_depth_axis,scan.N_azimuth_axis);
         x_matrix = reshape(scan.x,scan.N_depth_axis,scan.N_azimuth_axis);
         z_matrix = reshape(scan.z,scan.N_depth_axis,scan.N_azimuth_axis);
+        dirtest = scan.azimuth_axis(end)-scan.azimuth_axis(1);
         N_lines = scan.N_azimuth_axis;
     end
     % Mask out the valid delays within the "cone" in front of and after the transmit delay
     % The mask is calculated using the uff.apodization class before the TX delay loop.
     
+    
+    %quick fix to address cases where beam does not cover bottom of image
+    botinds = ~mask(end,:);
+    maxval = max( tx_delay(end,:).*mask(end,:) );
+    
     masked_delays = mask.*tx_delay;
+    mask(end,:) = true;
+    masked_delays(end, botinds) = maxval;
     
     % Interpolate the delays on the "edge" of the valid region
     % Yes, the code can probably be written more efficiently and intuitive
@@ -52,10 +61,12 @@ try
             weight_vector_2 = weight_vector_2./weight_vector_2(idx_a);
             interpolated_delay(:,x) = weight_vector_1.* tx_delay(idx_b,x) + weight_vector_2.* tx_delay(idx_a,x);
             
-        elseif  sum(masked_delays(z_idx_focus:end,x) == 0) & x_matrix(z_idx_focus,x) < source.x
+%         elseif  sum(masked_delays(z_idx_focus:end,x) == 0) & x_matrix(z_idx_focus,x) < source.x
+        elseif  sum(masked_delays(z_idx_focus:end,x) == 0) && (x_matrix(z_idx_focus,x)-source.x)*dirtest < 0
             last_before_idx = x;
             mask_before(:,x) = 1;
-        elseif  sum(masked_delays(z_idx_focus:end,x) == 0) & x_matrix(z_idx_focus,x) > source.x
+%         elseif  sum(masked_delays(z_idx_focus:end,x) == 0) & x_matrix(z_idx_focus,x) > source.x
+        elseif  sum(masked_delays(z_idx_focus:end,x) == 0) && (x_matrix(z_idx_focus,x)-source.x)*dirtest > 0
             if isempty(first_after_idx)
                 first_after_idx = x;
             end
