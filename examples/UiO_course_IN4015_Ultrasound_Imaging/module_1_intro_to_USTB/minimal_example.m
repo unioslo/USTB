03b85854cfe594995704de659b433782f2e0a378
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2022-08-26 11:54:58 +0200

    Info on workaround when downloading file

--- examples/UiO_course_IN4015_Ultrasound_Imaging/module_1_intro_to_USTB/minimal_example.m
+++ examples/UiO_course_IN4015_Ultrasound_Imaging/module_1_intro_to_USTB/minimal_example.m
@@ -4,6 +4,11 @@ filename='Verasonics_P2-4_parasternal_long_small.uff';
 tools.download(filename, url, local_path);
 channel_data = uff.read_object([local_path, filename],'/channel_data');
 
+% NB! If you have trouble downloading the data using the download tool you can 
+% download the data directly from https://ustb.no/datasets/Verasonics_P2-4_parasternal_long_small.uff
+% Delete the corrupt file with the same filename and move the downloaded data
+% to the "data/" folder in the USTB repository and rerun the example.
+
 depth_axis=linspace(0e-3,110e-3,1024).';                % Define image scan
 azimuth_axis=linspace(channel_data.sequence(1).source.azimuth,...
     channel_data.sequence(end).source.azimuth,channel_data.N_waves)';
