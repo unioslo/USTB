89dbdce9c8e2d70e05b3c3ad7fb21c728a0b593d
Stefano Fiorentini <stefano.fiorentini@ntnu.no>
2023-01-09 11:19:59 +0100

    Added sliding window feature in postprocess.coherent_compounding, as first suggested by Fabrice Prieur in issue 56. Modified proposed implementation to limit sliding window compounding to transmit dimension only

--- +postprocess/coherent_compounding.m
+++ +postprocess/coherent_compounding.m
@@ -1,23 +1,32 @@
 classdef coherent_compounding < postprocess
 %COHERENT_COMPOUNDING   Matlab implementation of Coherent compounding
+% If the window_size property is specified, a moving sum with window size
+% window_size is performed. Moving sum is only performed along the transmit
+% dimension. The receive dimension is compounded as usual
 %
 %   authors: Alfonso Rodriguez-Molares (alfonso.r.molares@ntnu.no)
 %            Ole Marius Hoel Rindal <olemarius@olemarius.net>
+%            Fabrice Prieur
+%            Stefano Fiorentini <stefano.fiorentini@ntnu.no>
 %
-%   $Last updated: 2017/09/10$
+%   $Last updated: 2022/09/01$
 
     %% constructor
     methods (Access = public)
         function h=coherent_compounding()
             h.name='Coherent compounding MATLAB';   
             h.reference='www.ntnu.no';                
-            h.implemented_by={'Alfonso Rodriguez-Molares <alfonso.r.molares@ntnu.no>','Ole Marius Hoel Rindal <olemarius@olemarius.net>'};   
-            h.version='v1.0.4';
+            h.implemented_by={'Alfonso Rodriguez-Molares <alfonso.r.molares@ntnu.no>', ...
+                'Ole Marius Hoel Rindal <olemarius@olemarius.net>', ...
+                'Fabrice Prieur', ...
+                'Stefano Fiorentini <stefano.fiorentini@ntnu.no>'};   
+            h.version='v1.1.0';
         end
     end
     
     properties (Access = public)
        dimension = dimension.both;          % Which "dimension" to sum over
+       window_size = []                     % If specified, do moving sum along transmit dimension
     end
     
     methods
@@ -28,24 +37,38 @@ classdef coherent_compounding < postprocess
                 return;
             end
             
-            [N_pixels Nrx Ntx N_frames]=size(h.input.data);
-
-            % declare output structure
-            h.output=uff.beamformed_data(h.input); % ToDo: instead we should copy everything but the data
+                        
+            % Define output object
+            h.output=uff.beamformed_data(h.input);
             
-            switch h.dimension
-                case dimension.both
-                    %out_data.data=zeros(N_pixels, 1, 1, N_frames);
-                    h.output.data=sum(sum(h.input.data,2),3);
-                case dimension.transmit
-                    %out_data.data=zeros(N_pixels, Nrx, 1, N_frames);
-                    h.output.data=sum(h.input.data,3);
-                case dimension.receive
-                    %out_data.data=zeros(N_pixels, 1, Ntx, N_frames);
-                    h.output.data=sum(h.input.data,2);
-                otherwise
-                    error('Unknown dimension mode; check HELP dimension');
-            end
+            if isempty(h.window_size)
+                switch h.dimension
+                    case dimension.both %#ok<*PROP>
+                        h.output.data=sum(h.input.data,[2,3]);
+                    case dimension.transmit
+                        h.output.data=sum(h.input.data,3);
+                    case dimension.receive
+                        h.output.data=sum(h.input.data,2);
+                    otherwise
+                        error('Unknown dimension mode; check HELP dimension');
+                end
+            else
+                assert(h.window_size <= h.input.N_waves, ...
+                'Cannot have a window size greater that the available number of transmit events.')
+            
+                data = reshape(h.input.data, [h.input.N_pixels, h.input.N_channels, 1, h.input.N_waves*h.input.N_frames]);
+                
+                switch h.dimension
+                    case dimension.both
+                        h.output.data = movsum(sum(data, 2), h.window_size, 4, 'Endpoints', 'discard');
+                    case dimension.transmit
+                        h.output.data=movsum(data, h.window_size, 4, 'Endpoints', 'discard');
+                    case dimension.receive
+                        h.output.data=sum(h.input.data,2);
+                    otherwise
+                        error('Unknown dimension mode; check HELP dimension');
+                end
+            end            
             
             % pass reference
             output = h.output;
@@ -53,6 +76,19 @@ classdef coherent_compounding < postprocess
             % update hash
             h.save_hash();
 
+        end
+    end
+    
+    methods
+        function set.dimension(h, val)
+            validateattributes(val, {'dimension'}, {'scalar'})
+            h.dimension = val;
+        end
+        function set.window_size(h, val)
+            if ~isempty(val)
+                validateattributes(val, {'single', 'double'}, {'integer', 'scalar'})
+            end
+            h.window_size = val;
         end
     end
 end
