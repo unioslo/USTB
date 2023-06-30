a1c043973befd0e723204889618dd3972ee9acf7
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2023-06-30 11:40:13 +0200

    Update run_tests.sh
    Remove token

--- @unit_test/run_tests.sh
+++ @unit_test/run_tests.sh
@@ -9,12 +9,6 @@ export USTB_REPO=$WORK_DIR/ustb
 MATLAB_ROOT=$PWD/../
 export MATLABPATH=$MATLAB_ROOT
 
-if [ -z "$SONAIR_TOKEN" ]; then
-    echo ""
-    echo "**Warning SONAIR_TOKEN is not set - tests involving reading data from database will fail **"
-    echo ""
-fi
-
 echo "Hello from the server!"
 
 rm -rf $WORK_DIR
