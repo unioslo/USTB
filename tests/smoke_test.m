46233c75cd9f7548ff271a8c1a3c490783b4a027
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2026-03-16 20:15:22 +0100

    Fix formatting

--- tests/smoke_test.m
+++ tests/smoke_test.m
@@ -1,13 +1,13 @@
-classdef SmokeTest < matlab.unittest.TestCase
+classdef smoke_test < matlab.unittest.TestCase
 
     methods (Test)
-        function testUstbPathExists(testCase)
+        function test_ustb_path_exists(testCase)
             p = ustb_path();
             testCase.verifyTrue(isfolder(p), ...
                 'ustb_path() should return a valid directory');
         end
 
-        function testUffScanClassExists(testCase)
+        function test_uff_scan_class_exists(testCase)
             s = uff.scan();
             testCase.verifyClass(s, ?uff.scan);
         end
